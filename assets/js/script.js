eb8a67c6aa1a2d1ec54bf22a6ea0ca2dca7d1441
Spread.design <hello@spread.ooo>
2025-03-11 16:37:57 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,78 +9,66 @@ function mobmenuclick() {
     $('.mob-icon-right').toggleClass('toggleRight-alt')
 }
 
-// function dragcard1() {
-//     $('#mob-card-1').css({ display: 'none' })
-//     $('#mob-card-2').css({ display: 'block' })
-//     $('#mob-card-3').css({ display: 'none' })
-//     $('#md-dot-1').removeClass('md-active')
-//     $('#md-dot-2').addClass('md-active')
-//     $('#md-dot-3').removeClass('md-active')
-// }
-
-// function dragcard2() {
-//     $('#mob-card-1').css({ display: 'none' })
-//     $('#mob-card-2').css({ display: 'none' })
-//     $('#mob-card-3').css({ display: 'block' })
-//     $('#md-dot-1').removeClass('md-active')
-//     $('#md-dot-2').removeClass('md-active')
-//     $('#md-dot-3').addClass('md-active')
-// }
-
-// function dragcard3() {
-//     $('#mob-card-1').css({ display: 'block' })
-//     $('#mob-card-2').css({ display: 'none' })
-//     $('#mob-card-3').css({ display: 'none' })
-//     $('#md-dot-1').addClass('md-active')
-//     $('#md-dot-2').removeClass('md-active')
-//     $('#md-dot-3').removeClass('md-active')
-// }
-
-function clickdot1() {
-    $('#mob-card-1').css({ display: 'none' })
-    $('#mob-card-2').css({ display: 'block' })
-    $('#mob-card-3').css({ display: 'none' })
-    $('#md-dot-1').removeClass('md-active')
-    $('#md-dot-2').addClass('md-active')
-    $('#md-dot-3').removeClass('md-active')
-}
-
-function clickdot2() {
+function mobarrowleft1() {
     $('#mob-card-1').css({ display: 'none' })
     $('#mob-card-2').css({ display: 'none' })
     $('#mob-card-3').css({ display: 'block' })
-    $('#md-dot-1').removeClass('md-active')
-    $('#md-dot-2').removeClass('md-active')
-    $('#md-dot-3').addClass('md-active')
+    $('#mobarrowleft-3').css({ opacity: '1' })
+    $('#mobarrowright-3').css({ opacity: '50%' })
 }
 
-function clickdot3() {
+function mobarrowleft2() {
     $('#mob-card-1').css({ display: 'block' })
     $('#mob-card-2').css({ display: 'none' })
     $('#mob-card-3').css({ display: 'none' })
-    $('#md-dot-1').addClass('md-active')
-    $('#md-dot-2').removeClass('md-active')
-    $('#md-dot-3').removeClass('md-active')
+    $('#mobarrowleft-1').css({ opacity: '50%' })
+    $('#mobarrowright-1').css({ opacity: '1' })
+}
+
+function mobarrowleft3() {
+    $('#mob-card-1').css({ display: 'none' })
+    $('#mob-card-2').css({ display: 'block' })
+    $('#mob-card-3').css({ display: 'none' })
+    $('#mobarrowleft-2').css({ opacity: '1' })
+    $('#mobarrowright-2').css({ opacity: '1' })
+}
+
+function mobarrowright1() {
+    $('#mob-card-1').css({ display: 'none' })
+    $('#mob-card-2').css({ display: 'block' })
+    $('#mob-card-3').css({ display: 'none' })
+    $('#mobarrowleft-2').css({ opacity: '1' })
+    $('#mobarrowright-2').css({ opacity: '1' })
+}
+
+function mobarrowright2() {
+    $('#mob-card-1').css({ display: 'none' })
+    $('#mob-card-2').css({ display: 'none' })
+    $('#mob-card-3').css({ display: 'block' })
+    $('#mobarrowleft-3').css({ opacity: '1' })
+    $('#mobarrowright-3').css({ opacity: '50%' })
+}
+
+function mobarrowright3() {
+    $('#mob-card-1').css({ display: 'block' })
+    $('#mob-card-2').css({ display: 'none' })
+    $('#mob-card-3').css({ display: 'none' })
+    $('#mobarrowleft-1').css({ opacity: '50%' })
+    $('#mobarrowright-1').css({ opacity: '1' })
 }
 
 function mobcardclick1() {
     $('#mob-img-1').css({ display: 'none' })
     $('#mob-sub-img-1').css({ display: 'block' })
-    // $('#mob-img-1').attr('src','/assets/images/GOX-mob.png')
     $('#mob-subcard-1').css({ display: 'none' })
     $('#mob-submaincard-1').css({ display: 'block' })
     $('.mob-dots').css({ display: 'none' })
     $('#mob-sub-arrow-1').css({ display: 'block' })
-    // $('#mob-arrow-1').addClass('mobRotate')
-    // if($('#mob-arrow-1').hasClass('mobRotate')) {
-    //     $('#mob-arrow-1').css({ transform: 'rotate(-45deg)', top: '-5em', right: '-5em', transition: '2s ease' })
-    // }
 }
 
 function mobcardhide1() {
     $('#mob-img-1').css({ display: 'block' })
     $('#mob-sub-img-1').css({ display: 'none' })
-    // $('#mob-img-1').attr('src','/assets/images/GOX-mob-crop.png')
     $('#mob-subcard-1').css({ display: 'flex' })
     $('#mob-submaincard-1').css({ display: 'none' })
     $('.mob-dots').css({ display: 'flex' })
@@ -152,16 +140,3 @@ function mobmaincardhide3() {
     $('#mob-maincard-3').css({ display: 'none' })
     $('#mob-card-3').css({ display: 'block' })
 }
-
-//  id="mob-arrow-1" onmouseenter="mobarrowmouseenter()" onmouseleave="mobarrowmouseleave()"
-// function mobarrowmouseenter() {
-//     $('#mob-arrow-1').css({ transform: 'rotate(-45deg)' })
-//     $('#mob-arrow-2').css({ transform: 'rotate(-45deg)' })
-//     $('#mob-arrow-3').css({ transform: 'rotate(-45deg)' })
-// }
-
-// function mobarrowmouseleave() {
-//     $('#mob-arrow-1').css({ transform: 'rotate(0deg)' })
-//     $('#mob-arrow-2').css({ transform: 'rotate(0deg)' })
-//     $('#mob-arrow-3').css({ transform: 'rotate(0deg)' })
-// }
